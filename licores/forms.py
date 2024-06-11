da6509856e72161c8c80c03cbf6048a6a0e5e1db
art2405 <arturodragon.ar@gmail.com>
2024-06-11 12:52:40 -0600

    Punto de control

--- licores/forms.py
+++ licores/forms.py
@@ -9,5 +9,14 @@ class ResenhaForm(forms.ModelForm):
 class LicorForm(forms.ModelForm):
     class Meta:
         model = Licor
-        fields = ['nombre', 'marca', 'cantidad','descripcion', 'precio', 'imagen', 'categorias']
+        fields = ['nombre', 'marca', 'cantidad', 'descripcion', 'precio', 'imagen', 'categorias']
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'marca': forms.TextInput(attrs={'class': 'form-control'}),
+            'cantidad': forms.NumberInput(attrs={'class': 'form-control'}),
+            'descripcion': forms.Textarea(attrs={'class': 'form-control'}),
+            'precio': forms.NumberInput(attrs={'class': 'form-control'}),
+            'imagen': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'categorias': forms.SelectMultiple(attrs={'class': 'form-control'}),
+        }
 
